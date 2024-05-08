a28a9e86feea34ae62391fdac82e056d85ee4b92
dhiv <vetri93selvi@gmail.com>
2024-05-08 18:21:45 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int num = 10;
-        long factorial = 1;
-        for (int i = 1; i <= num; ++i) {
-            // factorial = factorial * i;
-            factorial *= i;
-        }
-        System.out.printf("Factorial of %d = %d", num, factorial);
+        // create a string
+        String greet = "Guvi Geek";
+        System.out.println("String: " + greet);
+
+        // get the length of greet
+        int length = greet.length();
+        System.out.println("Length: " + length);
     }
 }
