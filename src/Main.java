4fc0cade0c58a8f05850e8b19227b4ebef02d7fc
dhiv <vetri93selvi@gmail.com>
2024-05-08 18:18:51 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int num = 29;
-        boolean flag = false;
-        for (int i = 2; i <= num / 2; ++i) {
-            // condition for nonprime number
-            if (num % i == 0) {
-                flag = true;
-                break;
-            }
+        int num = 10;
+        long factorial = 1;
+        for (int i = 1; i <= num; ++i) {
+            // factorial = factorial * i;
+            factorial *= i;
         }
-
-        if (!flag)
-            System.out.println(num + " is a prime number.");
-        else
-            System.out.println(num + " is not a prime number.");}
-
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
 }
