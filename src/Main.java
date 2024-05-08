5eba3499dce5f04121c4d55f52b68df829ef4feb
dhiv <vetri93selvi@gmail.com>
2024-05-08 18:01:41 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("a,b,c,d,e,f,g,h,i,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z");
+        int x = 100, y = 200;
+
+        System.out.println("Before Swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        // Swapping using three
+        // Variables
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("After swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
     }
 }
