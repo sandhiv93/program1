30a215907523998cc86fc4539618ac619deada80
dhiv <vetri93selvi@gmail.com>
2024-05-09 14:41:50 +0530

    seventh commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // create a string
-        String greet = "Guvi Geek";
-        System.out.println("String: " + greet);
-
-        // get the length of greet
-        int length = greet.length();
-        System.out.println("Length: " + length);
+        System.out.print("welcome to guvi\n".repeat(10));
     }
 }
